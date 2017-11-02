43dbd5c3f19658f79f5ab0f141fe090de3030ee6
azure <1410725@hcmut.edu.vn>
2017-11-02 15:01:05 +0700

    New pin configurations

---
+++ LITVE_Library/include/litve_adc.h
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file    litve_adc.h
+  * @author  Azure
+  * @version V0.1.0
+  * @date    2nd-November-2017
+  * @brief 	 Configuration file for Line-tracking vehicle ADC data acquisition.
+  *
+  *
+  ******************************************************************************
+*/
+
+#ifndef INCLUDE_LITVE_ADC_H_
+#define INCLUDE_LITVE_ADC_H_
+
+/**/
+#define configUSE_LITVE_LIB
+
+
+#endif /* INCLUDE_LITVE_ADC_H_ */
